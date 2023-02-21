a26c100a79033eda980c3c96b8c43147569a9044
Gourav2512 <gouravshetty2512@gmail.com>
2023-02-21 15:32:49 +0530

    scdscdccc

---
+++ src/main/java/basic/dlte/javapart/CalculateFD.java
@@ -0,0 +1,24 @@
+package basic.dlte.javapart;
+
+import java.util.Scanner;
+
+public class CalculateFD {
+    public static void main(String[] args) {
+        int a;
+        float b = 9.67f;
+        int c = (int)b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+//        Integer amount=500000, duration=3;
+//        Double maturiity=0.0, interest=6.4;
+//        Double temp=0.0;
+//        temp = (double)amount;
+//        while(duration>0){
+//            maturiity = (temp*interest/100);
+//            temp += maturiity;
+//            duration--;
+//        }
+       // System.out.println(temp);
+
+    }
+}
